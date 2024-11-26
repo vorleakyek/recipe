80921a6a19d519fea66e405ade01e767630c164f
Vorleak Yek <vorleakyek@gmail.com>
2024-11-26 10:53:49 -0800

    update navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,11 +1,17 @@
 import Link from "next/link";
 import Image from "next/image";
 import logo from "../public/logo.webp";
+import {Search, Menu} from "lucide-react";
 
 const Navbar = () => {
   return (
-    <header className="pt-5">
-      <nav>
+    <header>
+      <nav className="flex justify-between items-center">
+        <div>
+          <button>
+            <Menu />
+          </button>
+        </div>
         <div className="flex justify-center items-center">
           <Link href="/">
             <Image src={logo} alt="logo" width={50} height={50} />
@@ -14,7 +20,13 @@ const Navbar = () => {
             <h1 className="font-bold ml-3">Fusion of Flavor</h1>
           </Link>
         </div>
+        <div>
+          <button>
+            <Search/>
+          </button>
+        </div>
       </nav>
+      <hr className="mt-2" style={{ border:'1px solid black'}}/>
     </header>
   )
 }
