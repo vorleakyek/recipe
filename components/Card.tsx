ac085af9aae5f64186cc3770d8feb3adf3a391e8
Vorleak Yek <vorleakyek@gmail.com>
2024-11-26 15:23:24 -0800

    update changes in card

--- components/Card.tsx
+++ components/Card.tsx
@@ -1,14 +1,28 @@
 
 import Image from "next/image";
-import {Heart} from "lucide-react";
-export default function Card({src, title}:{src:string, title:string}) {
-  return(
+import { Heart } from "lucide-react";
+import StarRating from "./StarRating";
+
+interface CardProps {
+  src: string;
+  title: string;
+  rating: number;
+  totalRating: number;
+}
+export default function Card({ src, title, rating, totalRating }: CardProps) {
+  return (
     <>
-      <div className="relative">
-        <Image src={src} alt="image" width={145} height={145} />
-        <Heart className="absolute inset-0 w-7 h-7 fill-white stroke-red-600 " />
+      <div className="basis-6/12 mb-5">
+        <div className="relative">
+          <Image src={src} alt="image" width={145} height={145} />
+          <Heart className="absolute inset-0 w-7 h-7 fill-white stroke-red-600 " />
+        </div>
+        <p className="mt-2 pr-3 text-ellipsis overflow-hidden">{title}</p>
+        <div>
+          <StarRating rating={rating} />
+          <p>{totalRating} Ratings</p>
+        </div>
       </div>
-      <p>{title}</p>
     </>
   )
 }
